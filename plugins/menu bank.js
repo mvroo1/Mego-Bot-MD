16c0ceca06e70406b6cf25d7ec6199c09ea3ba1f
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-06-14 22:48:53 +0300

    Update menu bank.js

--- plugins/menu bank.js
+++ plugins/menu bank.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let videoUrl = 'https://telegra.ph/file/65cfc6dd32215a624e9dc.mp4'
+  let videoUrl = 'https://telegra.ph/file/516515c1a861abdb0e7dd.mp4'
   let { name } = global.db.data.users[who]
   m.react('💎')
 let str = `                  ✥━─━⌬ 𝑮𝒐𝒌𝒖_𝒃𝒐𝒕 ⌬━─━✥
@@ -18,6 +18,10 @@ let str = `                  ✥━─━⌬ 𝑮𝒐𝒌𝒖_𝒃𝒐𝒕 ⌬�
 ❐╎📝❯ .تسجيل⌉
 ❐╎📃❯ .الغاء_التسجيل⌉
 ❐╎🎒❯ .حقيبه⌉
+❐╎🏭❯ .صناعة⌉
+❐╎🏬❯ .متجر⌉
+❐╎⚜️❯ .تحقق⌉
+
                     ✥━─━⌬ 𝑮𝒐𝒌𝒖_𝒃𝒐𝒕 ⌬━─━✥
 `
   conn.sendMessage(m.chat, {
