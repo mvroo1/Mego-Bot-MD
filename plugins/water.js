07ca0878a385d720051ce4a81880aa76676e8fef
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-06-14 21:55:42 +0300

    Update water.js

--- plugins/water.js
+++ plugins/water.js
@@ -1,6 +1,7 @@
 const dir = [
 'https://telegra.ph/file/0304a6867441e70f99a3d.mp4',
-
+'https://telegra.ph/file/bf109580de41a9f794892.mp4', 
+  
 ];
 let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dado.webp', '', m)
