48ba3bee2b5c95a0be1ecd85031edfdfd9379a1f
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-06-14 21:52:06 +0300

    Update owr.js

--- plugins/owr.js
+++ plugins/owr.js
@@ -1,11 +1,9 @@
-/* ⚠ POR FAVOR NO MODIFIQUES NADA DE AQUÍ ⚠ */
-
-import { generateWAMessageFromContent } from '@adiwajshing/baileys'
-import fs from 'fs'
-let handler = async (m, { conn, usedPrefix, command }) => {
-let name = await conn.getName(m.sender)
-let donar =`
-*┏ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━*
+let handler = async (m, { conn }) => {
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let videoUrl = 'https://telegra.ph/file/9ffe02eb52dd5a0fd3619.png'
+  let { name } = global.db.data.users[who]
+  m.react('✍🏻')
+let str = `                 *┏ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━*
 *┇          「 المطور 」*
 *┣ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━*
 *┃ مرحبا ${name}*
@@ -14,7 +12,7 @@ let donar =`
 *┃ في حالة دعمك :𝟹*
 *┃*
 *┃ ➤ رقم المطور:* 
-*┃ +967774318278*
+*┃ https://wa.me/967774318278*
 *┃ ➤ بلد: اليمن* 
 *┃ ➤ يعمل البوت بنظام: لينكس* 
 *┃ ➤ متاح: 24 ساعه*  
@@ -23,13 +21,18 @@ let donar =`
 *┃ 👉🏻 اذا كان لديك مشكله راسلني علي رقمي*
 *┃ تعال فقط اذا كان لديك مشكله فقط*
 *┃ wa.me/+967774318278*
-*┗ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━*
-`.trim()
-let aa = { quoted: m, userJid: conn.user.jid }
-let res = generateWAMessageFromContent (m.chat, {liveLocationMessage: {degreesLatitude: 0, degreesLongitude: 0, caption: donar, secuenceNumber: "0", contextInfo: {mentionedJid: conn.parseMention()}}}, aa)
-conn.relayMessage(m.chat, res.message, {})
-}
-handler.help = ['donasi']
-handler.tags = ['info']
-handler.command = /^owner|مطور|ower$/i
+*┗ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━* 
+                    *✥━─━⌬ 𝑮𝒐𝒌𝒖_𝒃𝒐𝒕 ⌬━─━✥*
+`
+  conn.sendMessage(m.chat, {
+           video: { url: videoUrl }, caption: str,
+     mentions: [m.sender,global.conn.user.jid],
+     gifPlayback: true,gifAttribution: 0
+       }, { quoted: m });
+   };
+
+handler.help = ['main']
+handler.tags = ['group']
+handler.command = ['مطور']
+
 export default handler
