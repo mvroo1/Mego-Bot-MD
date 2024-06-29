cd9b42d16c47ebea80487dcd767ba2c9feac6358
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-06-29 19:50:04 +0300

    Update فعاليه.js

--- plugins/فعاليه.js
+++ plugins/فعاليه.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
         conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
         throw false
     }
-    let src = await (await fetch('https://gist.githubusercontent.com/Kyutaka101/2ca0f797f66d524a1f2e3685f66298f2/raw/7fe3898420592b47ca37db07fdd173b9b4ca9a63/Game.json')).json()
+    let src = await (await fetch('https://github.com/Amrobraih/amro/blob/master/src/game/Game.json')).json()
   let json = src[Math.floor(Math.random() * src.length)]
     let caption = `*· • • ━━ ⌝🐉⌞ ━━ • • ·*
 *${command.toUpperCase()}*
