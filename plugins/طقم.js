a9efa9f86ff1f3d326080e686ddb73f51cb6475b
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-06-29 19:35:16 +0300

    Update طقم.js

--- plugins/طقم.js
+++ plugins/طقم.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch"
 let handler = async (m, { conn }) => {
 
-  let data = await (await fetch('https://raw.githubusercontent.com/KazukoGans/database/main/anime/ppcouple.json')).json()
+  let data = await (await fetch('https://github.com/Amrobraih/amro/blob/master/src/game2/anime.json')).json()
   let cita = data[Math.floor(Math.random() * data.length)]
   
   let cowi = await(await fetch(cita.cowo)).buffer()
