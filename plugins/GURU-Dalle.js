74837ad203636bbcc175dde5c0596c8cf3e9b792
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-06-29 19:28:54 +0300

    Update GURU-Dalle.js

--- plugins/GURU-Dalle.js
+++ plugins/GURU-Dalle.js
@@ -1,44 +1,26 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `*هذا الأمر يقوم بإنشاء صور من المطالبات النصية*\n\n*مثال*\n*◉ ${usedPrefix + command} anime Sukuna*\n*◉ ${usedPrefix + command} anime cat*`;
+  if (!text) throw `*يقوم هذا الامر بصناعه صوره ب الذكاء الاصطناعي *\n\n*مثال للأستخدام (لضمان صوره دقيقه اكتب ب الانجليزيه)*\n*◉ ${usedPrefix + command} بنت انمي جميله*\n*◉ ${usedPrefix + command} رجل يقود بيتزا*`;
 
   try {
-    m.reply('*الرجاء الانتظار، جاري إنشاء الصور...*');
+    m.reply('*ثانيه الصوره بتجهز...*');
 
-    const apiKey = 'sk-proj-mgjXcRDD15i4IcSovoeiT3BlbkFJNDrKzjZ7PmZkv73oOtyE'; // استبدل هذا بالقيمة الصحيحة لمفتاح OpenAI API الخاص بك
-    const endpoint = 'https://api.openai.com/v1/images/generations';
-
-    const response = await fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${apiKey}`,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        model: 'image-alpha-001',
-        prompt: text,
-        num_images: 1,
-        size: '512x512'
-      })
-    });
+    const endpoint = `https://gurugpt.cyclic.app/dalle?prompt=${encodeURIComponent(text)}`;
+    const response = await fetch(endpoint);
 
     if (response.ok) {
-      const data = await response.json();
-      const imageUrl = data.data[0].url;
-      await conn.sendFile(m.chat, imageUrl, 'image.png', null, m);
+      const imageBuffer = await response.buffer();
+      await conn.sendFile(m.chat, imageBuffer, 'image.png', null, m);
     } else {
-      const errorData = await response.json();
-      console.error('Error generating image:', errorData);
-      throw '*فشل في إنشاء الصورة*';
+      throw '*فشل إنشاء الصورة*';
     }
-  } catch (error) {
-    console.error('Error:', error);
-    throw '*أوه، حدث خطأ ما أثناء إنشاء الصورة. الرجاء المحاولة مرة أخرى في وقت لاحق.*';
+  } catch {
+    throw '*أُووبس!  حدث خطأ ما أثناء إنشاء الصور.  الرجاء معاودة المحاولة في وقت لاحق.*';
   }
 };
 
-handler.help = ['dalle'];
-handler.tags = ['AI'];
-handler.command = ['dalle', 'ارسم', 'رسم', 'openai2'];
+handler.help = ['صورهai'];
+handler.tags = ['tools'];
+handler.command = ['dalle', 'صورهai', 'ارسم', 'openai2'];
 export default handler;
